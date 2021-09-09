f3fba5903cf8db38d3deaf02754b37c16c7641fe
sashtje <sashtje.lefevre@gmail.com>
2021-09-09 20:34:41 +0300

    feat: add function for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,57 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  var sepFullAdd = "";
+  var sepForStr = "";
+  var result = "";
+  var separator = "+";
+  var addition;
+  var strWithNull;
+
+  if (options.repeatTimes == 0) return result;
+
+  if (options.addition !== undefined && options.additionRepeatTimes != 0) {
+    if (options.addition === null) addition = "null";
+    else addition = options.addition;
+    if (
+      options.additionRepeatTimes === undefined ||
+      options.additionRepeatTimes == 1
+    )
+      sepFullAdd = addition;
+    else {
+      var additionSeparator = "|";
+      var arr = [];
+      if (options.additionSeparator != undefined)
+        additionSeparator = options.additionSeparator;
+      for (var i = 0; i < options.additionRepeatTimes; i++) arr.push(addition);
+      sepFullAdd = arr.join(additionSeparator);
+    }
+  }
+
+  if (options.separator !== undefined) separator = options.separator;
+  sepForStr = sepFullAdd + separator;
+
+  var resArr = [];
+  if (str === null) strWithNull = "null";
+  else strWithNull = str;
+  if (options.repeatTimes === undefined) resArr.push(strWithNull);
+  else {
+    for (var i = 0; i < options.repeatTimes; i++) resArr.push(strWithNull);
+  }
+  return resArr.join(sepForStr) + sepFullAdd;
 }
